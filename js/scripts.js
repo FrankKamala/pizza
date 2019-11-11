277f2a16d0cf42ee8212b64ed430aadf026452d7
FrankKamala <frankkamala@gmail.com>
2019-11-11 12:27:11 +0300

    added pricing logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,34 @@
 $(document).ready(function() {
   // prices
-  let small=500, medium=750, large= 950 ;
+  function Pizza(flavor ,toppings,size, quantity) {
+
+    this.flavor = flavor;
+    this.toppings = toppings;
+    this.size = size;
+    this.quantity = quantity;
+
+   }
+
 let btn=document.querySelector('#order');
 btn.addEventListener('click',function(){
 
 
 //Get pizza inputs
-flavor=document.querySelector('#flavor').value;
-toppings=document.querySelector('#toppings').value;
-size=document.querySelector('#size').value;
+flavor=document.querySelector('#flavor ').value;
+toppings=document.querySelector('#toppings ').value;
+size=document.querySelector('#size ').value;
 quantity=document.querySelector('#quantity').value;
+price = get_price();
 
-const order= new Pizza(flavor,toppings,size,quantity,price);
+
+const order= new Pizza(flavor,toppings,size,quantity,);
 $("#order").click(
   function(){
     $("#payment").text(order.flavor);
     $("#payment1").text(order.toppings);
     $("#payment2").text(order.size);
     $("#payment3").text(order.quantity);
+    $("#total").text(price);
   }
 );
 
@@ -28,20 +39,26 @@ $("#order").click(
 });
 
 // Store piza attributes
-function Pizza(flavor ,toppings,size, quantity) {
 
-  this.flavor = flavor;
-  this.toppings = toppings;
-  this.size = size;
-  this.quantity = quantity;
 
- }
- Pizza.prototype.fullOrder= function(){
-   return this.flavor + "," +this.toppings + "," + this.size + "," +this.quantity;
- }
- function price(small,medium,large) {
+ //price Function
+ let small=500, medium=750, large= 950 ;
+ let toppings =100;
+ let get_price =() => {
+   let price;
+   if (size === "small"){
+     price = small*quantity;
+     //console.log(price);
+     return price*quantity;
+   }else if (size ==="medium"){
+     price = medium *quantity;
+     //console.log(price);
+     return price*quantity;
+   }else if(size === "large"){
+     price = large*quantity;
+    // console.log(price);
 
-   this.small = small;
-   this.toppings = medium;
-   this.size =large;
+     return price ;
+   }
+
   }
