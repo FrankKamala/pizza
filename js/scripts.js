1f7a8c32bf65140e16223480cb2f9671ac595251
FrankKamala <frankkamala@gmail.com>
2019-11-11 16:44:05 +0300

    rearrange code and add table in htm

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,53 @@
+function Pizza(flavor ,toppings,size, quantity, price) {
+  this.flavor = flavor;
+  this.toppings = toppings;
+  this.size = size;
+  this.quantity = quantity;
+  this.price = price;
+}
+
+var small=500;
+var medium=750;
+var large= 950;
+var topping =100;
+
+
 $(document).ready(function() {
-  // prices
-  function Pizza(flavor ,toppings,size, quantity) {
-
-    this.flavor = flavor;
-    this.toppings = toppings;
-    this.size = size;
-    this.quantity = quantity;
-
-   }
-
-let btn=document.querySelector('#order');
-btn.addEventListener('click',function(){
-
-
-//Get pizza inputs
-flavor=document.querySelector('#flavor ').value;
-toppings=document.querySelector('#toppings ').value;
-size=document.querySelector('#size ').value;
-quantity=document.querySelector('#quantity').value;
-price = get_price();
-
-
-const order= new Pizza(flavor,toppings,size,quantity,);
 $("#order").click(
   function(){
-    $("#payment").text(order.flavor);
-    $("#payment1").text(order.toppings);
-    $("#payment2").text(order.size);
-    $("#payment3").text(order.quantity);
-    $("#total").text(price);
+
+    // Getting values from the form
+    flavor=$('#flavor').val();
+    toppings=$('#toppings').val();
+    size=$('#size').val();
+    quantity=parseInt($('#quantity').val());
+
+    // Getting the total amount
+    if (size == "small" && toppings == ""){
+      var price = small * quantity;
+    }
+    else if (size == "small" && toppings == "Extra tomato" || toppings == "Extra pineapple"){
+      var price = small * quantity + topping;
+    }if(size == "medium" && toppings =="" ){
+      var price = medium * quantity;
+    }else if(size == "medium" && toppings == "Extra tomato" || toppings == "Extra pineapple"){
+      var price = medium * quantity + topping;
+    }if (size == "large" && toppings == ""){
+      var price = large * quantity;
+    }else if (size == "large" && toppings =="Extra tomato" || toppings == "Extra pineapple") {
+      var price= large * quantity + topping;
+    }
+
+    var newPizza = new Pizza(flavor,toppings,size,quantity,price);
+    var newRow = '<tr><th id="flav">' + newPizza.flavor + '</th><td id="tp">' + $("#toppings option:selected").text() + " - " + newPizza.toppings + '</td><td id="sz">' + $("#size option:selected").text() + " - " + newPizza.size + '</td><td id="bxs">' + $("#quantity option:selected").text() + " - " + newPizza.quantity + '</td><td id="total">' + newPizza.price + '</td></tr>'
+$("#pizza").append(newRow);
+
+
+
+
   }
 );
 
 
 
 });
-
-});
-
-// Store piza attributes
-
-
- //price Function
- let small=500, medium=750, large= 950 ;
- let toppings =100;
- let get_price =() => {
-   let price;
-   if (size === "small"){
-     price = small*quantity;
-     //console.log(price);
-     return price*quantity;
-   }else if (size ==="medium"){
-     price = medium *quantity;
-     //console.log(price);
-     return price*quantity;
-   }else if(size === "large"){
-     price = large*quantity;
-    // console.log(price);
-
-     return price ;
-   }
-
-  }
